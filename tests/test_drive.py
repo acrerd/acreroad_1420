2f0bec4178c91321dc33f73c3d6a8aa6ec1879d2
Daniel Williams <mail@daniel-williams.co.uk>
2015-07-21 17:15:44 +0100

    Off to a start. Even with tests!

---
+++ tests/test_drive.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_drive
+-----------------
+Tests for the acreroad_1420.drive module
+"""
+
+
+import unittest
+
+from acreroad_1420 import drive
+
+class TestDrive(unittest.TestCase):
+    def setUp(self):
+        self.connection = drive.Drive('/dev/tty.usbserial', 9600, simulate=1)
+
+    def testCalibrate(self):
+        self.assertEqual(self.connection.calibrate(), ">c 000 000")
+
+    def testCalibrateWithValues(self):
+        self.assertEqual(self.connection.calibrate("450 650"), 1)
+
+    def testClockSet(self):
+        self.assertEqual(self.connection.setTime(), 1)
+
+    def testLocationSet(self):
+        self.assertEqual(self.connection.setLocation(), 1)
+
+    def testGotoSingle(self):
+        from astropy.coordinates import SkyCoord
+        from astropy.coordinates import ICRS, Galactic, FK4, FK5
+        from astropy.coordinates import Angle, Latitude, Longitude
+        import astropy.units as u
+
+        c = SkyCoord("galactic", l="1h12m43.2s", b="+1d12m43s")
+
+        self.assertEqual(self.connection.goto(c), 1)
+        
+        
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
