0ed8932d65074d096591f4b8bcb1639e8b8abe53
Daniel Williams <mail@daniel-williams.co.uk>
2016-03-08 17:51:59 +0000

    Updated docs.

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,8 @@
 import sys
 import os
 
+import sphinx_bootstrap_theme
+
 # If extensions (or modules to document with autodoc) are in another
 # directory, add these directories to sys.path here. If the directory is
 # relative to the documentation root, use os.path.abspath to make it
@@ -112,20 +114,21 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'default'
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-else:
-    sys.path.insert(0, os.path.abspath('../../'))
-    sys.path.insert(0, os.path.abspath('../acreroad_1420'))
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
+html_theme_options = {
+    
+    # Render the next and previous page links in navbar. (Default: true)
+    'navbar_sidebarrel': False,
+}
+
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -152,6 +155,7 @@ else:
 # static files, so a file named "default.css" will overwrite the builtin
 # "default.css".
 html_static_path = ['_static']
+templates_path = ['_templates']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page
 # bottom, using the given strftime format.
@@ -163,6 +167,9 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
+render_sidebar = True
+html_sidebars = {'**': ['localtoc.html', 'sourcelink.html', 'searchbox.html']}
+
 
 # Additional templates that should be rendered to pages, maps page names
 # to template names.
